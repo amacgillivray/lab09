47442151fbbda9c458277a3b8e9d0e4e3f562b39
Andrew MacGillivray <agjmacgill@gmail.com>
2022-04-27 20:45:19 -0500

    progress

--- ex02.php
+++ ex02.php
@@ -4,7 +4,11 @@
         <title>Ex. 2</title>
     </head>
     <body>
-        <form><?php
+        <form method="post" action="./ex2handler.php">
+<?php
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
+        
 		$qs = [ "What was the goal of the Jacobite uprising?",
 			"What was the goal that inspired the Republican Party to first form?",
 			"What was Euromaidan (2014)?",
@@ -34,23 +38,26 @@
 			"Amelia Earhart",			
 			"Winston Churchill",
 			"Douglas MacArthur",
-			"Martin Luther King, Jr."
+			"Martin Luther King, Jr.",
 			"Dwight Eisenhower",
 			"Teddy Roosevelt",
 			"all of the above",
 			"a7" ];
-		
+        
 		for ($i = 0; $i < sizeof($qs); $i++)
 		{
 			print "<p>" . $qs[$i] . "</p>";
 			$opts = ${"q".$i};
-			for ($e = 0; $e < sizeof($opts)-1; $e++)
+            // print_r($opts);
+			for ($e = 0; $e < (sizeof($opts)-1); $e++)
 			{
-				print '<input type="radio" id="'. $opts[$e] . '" name="q' . $i .'" value ="' . $opts[$e] . '">';
-				print "<label for=\"" . $opts[$e] . "\"><br/>";
+				print '<input type="radio" id="q'. $i . 'a' $e . '" ' .
+                      'name="q' . $i .'a' . $e . '" value ="q' . $i . 'a' . $e . '" />';
+				// print '<label for="q'. $i . 'a' $e . '" >' . $opts[$e] . '</label><br/>';
 			}
-			// add input type hidden
-		}
+            // print '<input type="hidden" name="q' +$i + 'ans" value="'$opts[sizeof(opts)-1]'" />';
+        }
+
 	?>
 	</form>
     </body>
