0dd14ac5a8d30cf51d5b097abd2ea100d345fa62
Andrew MacGillivray <agjmacgill@gmail.com>
2022-04-27 20:17:41 -0500

    progress

--- ex01.php
+++ ex01.php
@@ -6,6 +6,13 @@
     <body><?php 
         print "<table>";
 
+	print "<tr><th>&nbsp;</th>";
+	for ($i = 1; $i <= 100; $i++)
+        {
+		print "<th>" . $i . "</th>";
+	}
+	print "</tr>";
+
         for ($i = 1; $i <= 100; $i++)
         {
             print "<tr>";
@@ -13,7 +20,7 @@
             for ($e = 1; $e <= 100; $e++)
             {
                 print "<td>";
-                print $e*i; 
+                print $e*$i; 
                 print "</td>";
             }
             print "</tr>";
--- ex02.php
+++ ex02.php
@@ -4,9 +4,54 @@
         <title>Ex. 2</title>
     </head>
     <body>
-        <fieldset>
-            
-        </fieldset>
-
+        <form><?php
+		$qs = [ "What was the goal of the Jacobite uprising?",
+			"What was the goal that inspired the Republican Party to first form?",
+			"What was Euromaidan (2014)?",
+			"Which flag did Hong Kong protesters fly while fighting for their democracy?",
+			"Which is the coolest 20th-century western historical figure?"];
+		$q0 = [ "The restoration of the House of Stuart to the British Throne.",
+			"The installation of William III and Mary II.",
+			"William Wallace.",
+			"THEY MAY TAKE OUR LIVES, BUT THEY WILL NEVER TAKE OUR FREEDOM.",
+			"a0"];
+		$q1 = [ "The upholding of slavery.",
+			"The abolition of slavery.",
+			"The elimination of income tax.",
+			"Adherence to the gold standard.",
+			"a1" ];
+		$q2 = [ "A pro-Russian revolt against the government of Georgia.",
+			"A pro-European revolt against the government of Russia.",
+			"A pro-western revolt against the government of China.",
+			"An anti-Russian revolt against the government of Ukraine.",
+			"a3" ];
+		$q3 = [ "The Hong Kong Flag.",
+			"The British Flag.",
+			"The American Flag.",
+			"The Chinese Flag.",
+			"a2" ];
+		$q4 = [ "Alan Turing",
+			"Amelia Earhart",			
+			"Winston Churchill",
+			"Douglas MacArthur",
+			"Martin Luther King, Jr."
+			"Dwight Eisenhower",
+			"Teddy Roosevelt",
+			"all of the above",
+			"a7" ];
+		
+		for ($i = 0; $i < sizeof($qs); $i++)
+		{
+			print "<p>" . $qs[$i] . "</p>";
+			$opts = ${"q".$i};
+			for ($e = 0; $e < sizeof($opts)-1; $e++)
+			{
+				print '<input type="radio" id="'. $opts[$e] . '" name="q' . $i .'" value ="' . $opts[$e] . '">';
+				print "<label for=\"" . $opts[$e] . "\"><br/>";
+			}
+			// add input type hidden
+		}
+	?>
+	</form>
     </body>
 </html>
